417fbee1cef360e042feb952d0e09e9ffe7e2115
Somdotta07 <jhilick.s@gmail.com>
2021-10-12 02:55:46 +0200

    Project structure and styling
    - Add html tags to give the structure
    -Add Hamburger mobile menu
    -Add styling till program page

---
+++ main.js
@@ -0,0 +1,8 @@
+/* eslint-disable linebreak-style */
+const toggler = document.querySelector('.menu__toggler');
+const menu = document.querySelector('.menu');
+
+toggler.addEventListener('click', () => {
+  toggler.classList.toggle('active');
+  menu.classList.toggle('active');
+});
